fb459c1548ee8b071bf9fffff85751047ef2d76c
Branden <branden.walter@outlook.com>
2024-04-08 14:17:12 -0700

    Update UpdatePhoto.ps1

--- ActiveDirectory/UpdateADUserProfilePic/UpdatePhoto.ps1
+++ ActiveDirectory/UpdateADUserProfilePic/UpdatePhoto.ps1
@@ -1,17 +1,30 @@
-
-$imagePath = ".\profile_picture.jpg"
-
-$profilePicture = [System.IO.File]::ReadAllBytes($imagePath)
-
-$users = Get-ADUser -Filter *
-
-foreach ($user in $users) {
-    # Check if the user has a value set for the Specific thumbnail photo attribute
+function UpdateUserProfilePicture($user) {
+    # If the user's profile picture attribute is empty or not set, update it
     if (-not $user.thumbnailPhoto) {
+
         Set-ADUser -Identity $user -Replace @{thumbnailPhoto=$profilePicture}
         Write-Host "Profile picture added for $($user.Name)"
     }
     else {
         Write-Host "Profile picture already exists for $($user.Name)"
     }
-}  
+}
+
+function Main {
+
+    # Define the path to the image file
+    $scriptDirectory = $PSScriptRoot
+    $imagePath = Join-Path -Path $scriptDirectory -ChildPath "profile_picture.jpg"
+
+    # Read the image file and convert it to a byte array
+    $profilePicture = [System.IO.File]::ReadAllBytes($imagePath)
+    
+    $users = Get-ADUser -Filter *
+    
+    foreach ($user in $users) {
+        
+        UpdateUserProfilePicture $user
+    }
+}
+
+Main
