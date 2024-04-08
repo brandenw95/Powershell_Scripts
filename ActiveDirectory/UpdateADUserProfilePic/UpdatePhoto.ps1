59bb377062a3806e8bca4a5bb066ea3af0e27c56
Branden <branden.walter@outlook.com>
2024-04-08 14:33:12 -0700

    Update UpdatePhoto.ps1

--- ActiveDirectory/UpdateADUserProfilePic/UpdatePhoto.ps1
+++ ActiveDirectory/UpdateADUserProfilePic/UpdatePhoto.ps1
@@ -1,4 +1,26 @@
-function UpdateUserProfilePicture($user) {
+# Author: Branden Walter
+# Date: April 8th 2024
+# ========================
+# Description: Changes the profile of users in Active Directory
+#
+#   !! Important!!
+#
+#   Image must adhere to the following spec:
+#       - Must be 96 x 96 pixels
+#       - Less than 100kb in file size
+#
+# ========================
+
+function UpdateUserProfilePicture{
+    param (
+        [Parameter]
+        [Microsoft.ActiveDirectory.Management.ADUser] $User
+    )
+    
+    $scriptDirectory = $PSScriptRoot
+    $imagePath = Join-Path -Path $scriptDirectory -ChildPath "seafirst.jpg"
+    $profilePicture = [System.IO.File]::ReadAllBytes($imagePath)
+
     # If the user's profile picture attribute is empty or not set, update it
     if (-not $user.thumbnailPhoto) {
 
@@ -9,17 +31,34 @@ function UpdateUserProfilePicture($user) {
         Write-Host "Profile picture already exists for $($user.Name)"
     }
 }
-
+function init_warning {
+    Write-Output "!!Important!!"
+    Write-Output "Image must adhere to the following spec:"
+    Write-Output "- Less than 100kb in file size"
+    Write-Output "- Must be 96 x 96 pixels"
+}
 function Main {
 
-    # Define the path to the image file
-    $scriptDirectory = $PSScriptRoot
-    $imagePath = Join-Path -Path $scriptDirectory -ChildPath "profile_picture.jpg"
 
-    # Read the image file and convert it to a byte array
-    $profilePicture = [System.IO.File]::ReadAllBytes($imagePath)
-    
-    $users = Get-ADUser -Filter *
+
+    # Test User 1
+    $testUser1 = New-Object PSObject -Property @{
+        Name = "John Doe"
+        # No thumbnail photo
+        thumbnailPhoto = $null 
+    }
+
+    #Test User 2
+    $testUser2 = New-Object PSObject -Property @{
+        Name = "Jane Smith"
+        # Existing thumbnail photo
+        thumbnailPhoto = [byte[]]@(1, 2, 3) 
+    }
+
+    $testUsers = @($testUser1, $testUser2)
+
+    $users = $testUsers
+    #$users = Get-ADUser -Filter *
     
     foreach ($user in $users) {
         
